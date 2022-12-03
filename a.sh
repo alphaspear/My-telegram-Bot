04d15907d71555cda363147d52d74c2a9ef53312
Abhilash Joshi <iabhilashjoshi@gmail.com>
2022-12-03 16:56:18 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,11 +1,4 @@
 echo 'Loading Script'
 sleep 3
-echo 'loading remote configs from Abhilashs_RPI_in_chennai..'
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-echo 'Starting Extraction...'
-sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
-echo 'Extraction Complete'
-sleep 2
-echo 'Sending Status to Abhilashs_RPI_in_chennai..'
-sleep 3
-echo 'DONE'
+ngrok config add-authtoken 1bjDo3w6WyHRtRl0UoWwmCUzuMy_3C6j7GGBHnn8HpDtstaS5
+
